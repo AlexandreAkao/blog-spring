dc20a60be7ac14a29eaf8ee2389a9e976dd7aa8f
Alexandre Akira <alexandreakiraakao@gmail.com>
2020-04-22 10:56:11 -0300

    Models, repository and service add

---
+++ src/main/java/br/com/blog/controllers/NoticiaController.java
@@ -0,0 +1,9 @@
+package br.com.blog.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/noticias")
+public class NoticiaController {
+}
